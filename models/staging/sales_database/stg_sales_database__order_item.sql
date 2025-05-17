ecea2c35c9796ebce45a5d982692e7babc96194b
charline <charline.thomasson@gmail.com>
2025-05-17 14:35:33 +0200

    update: column name in order_item model for price

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -5,8 +5,8 @@ select CONCAT(order_id, '_', product_id) AS order_item_id_y,
  1 AS column_1_y,
  seller_id_y,
  DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
- price as unit_price_y,
- shipping_cost_y,
+ price, -- error instered here : removed alias "AS unit_price_y"
+ shipping_cost_y, 
  quantity as item_quantity_y,
  (price * quantity) + shipping_cost as total_order_item_amount_y
 from {{ source('dataset_fil_rouge', 'order_item') }}
