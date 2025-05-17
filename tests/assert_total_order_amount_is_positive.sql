9dfc66e9a701196e2570c2dd4cb3e3942dfb5467
Cécile Pelletier <pelikanfanclub@gmail.com>
2025-05-17 15:29:26 +0000

    test dbt cloud fix erreurs

--- tests/assert_total_order_amount_is_positive.sql
+++ tests/assert_total_order_amount_is_positive.sql
@@ -1,6 +1,6 @@
 select
-    order_id,
+    order_id_y,
     sum(total_order_item_amount) as total_amount
 from {{ ref('stg_sales_database__order_item') }}
-group by order_id
+group by order_id_y
 having total_amount < 0
