9ffeca46405024a7c3aef325433bf23d9c9d7d67
Charline_T <79654866+Charline-Thomasson@users.noreply.github.com>
2025-05-17 14:12:49 +0000

    fix-price-column-name

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -4,7 +4,7 @@ select CONCAT(order_id, '_', product_id) AS order_item_id_y,
  1 AS column_1_y,
  seller_id_y,
  DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
- price as unit_price,
+ price as unit_price_y,
  shipping_cost_y, 
  quantity as item_quantity_y,
  (price * quantity) + shipping_cost as total_order_item_amount_y
